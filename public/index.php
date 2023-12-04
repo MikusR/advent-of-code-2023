1f96d58340b69a683275c40c25c2082ab9386a3e
MikusR <mikus.rakis@gmail.com>
2023-12-04 06:29:32 +0200

    Day 4 init

--- public/index.php
+++ public/index.php
@@ -2,12 +2,12 @@
 
 declare(strict_types=1);
 
-use App\Day03;
+use App\Day04;
 
 require_once __DIR__ . '/../vendor/autoload.php';
 
 $dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/../');
 $dotenv->safeLoad();
 
-$day = new Day03();
+$day = new Day04();
 $day->run();
