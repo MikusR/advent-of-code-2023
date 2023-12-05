fb99a206a51d99dab445c345e2c3751ffbdfbae8
MikusR <mikus.rakis@gmail.com>
2023-12-05 14:24:36 +0200

    Day 5 part 2

--- app/Day05.php
+++ app/Day05.php
@@ -19,45 +19,44 @@ class Day05
         $response = $client->request('GET', 'https://adventofcode.com/2023/day/5/input', ['cookies' => $jar]);
         echo '<pre>';
         $data = (string)$response->getBody();
-//        $data = explode('\n\n', $data);
-//        $data = preg_split('/\r\n|\r|\n/', $data);
-//        $data = 'seeds: 79 14 55 13
-//
-//seed-to-soil map:
-//50 98 2
-//52 50 48
-//
-//soil-to-fertilizer map:
-//0 15 37
-//37 52 2
-//39 0 15
-//
-//fertilizer-to-water map:
-//49 53 8
-//0 11 42
-//42 0 7
-//57 7 4
-//
-//water-to-light map:
-//88 18 7
-//18 25 70
-//
-//light-to-temperature map:
-//45 77 23
-//81 45 19
-//68 64 13
-//
-//temperature-to-humidity map:
-//0 69 1
-//1 0 69
-//
-//humidity-to-location map:
-//60 56 37
-//56 93 4';
+
+        $data2 = 'seeds: 79 14 55 13
+
+seed-to-soil map:
+50 98 2
+52 50 48
+
+soil-to-fertilizer map:
+0 15 37
+37 52 2
+39 0 15
+
+fertilizer-to-water map:
+49 53 8
+0 11 42
+42 0 7
+57 7 4
+
+water-to-light map:
+88 18 7
+18 25 70
+
+light-to-temperature map:
+45 77 23
+81 45 19
+68 64 13
+
+temperature-to-humidity map:
+0 69 1
+1 0 69
+
+humidity-to-location map:
+60 56 37
+56 93 4';
 
         $data = preg_split('/\n\n/', $data);
         $almanac = [];
-//        preg_match_all('(\d+)', $test[$card]['winners'], $matches);
+
         foreach ($data as $key => $d) {
             if ($key === 0) {
                 $temp = explode('seeds:', $d);
@@ -81,23 +80,42 @@ class Day05
                 }
             }
         }
-        foreach ($seeds as $seed) {
-            $destination[$seed] = $seed;
-            $dest = $seed;
-            foreach ($almanac as $key => $item) {
-                $found = false;
-                foreach ($item as $line) {
-                    if (($destination[$seed] >= $line['source']) &&
-                        ($destination[$seed] <= $line['source'] + $line['range'] - 1)) {
-                        $found = true;
-                        $dest = $line['destination'] + abs($line['source'] - $destination[$seed]);
+//        foreach ($seeds as $key => $seed) {
+//            if ($key % 2 === 0) {
+//                for ($i = 0; $i < $seeds[$key + 1]; $i++) {
+//                }
+//            }
+//        }
+        var_dump(opcache_get_status()['jit']);
+        $minimum = false;
+		
+        foreach ($seeds as $key => $seed) {
+			var_dump('seed:',$seed);
+            if ($key % 2 === 0) {
+                
+                for ($i = 0; $i < $seeds[$key + 1]; $i++) {
+                    $destination = $seed+$i;
+                    $dest = $destination;
+					//var_dump('seed:',$destination);
+                    foreach ($almanac as $item) {
+                        foreach ($item as $line) {
+                            if (($destination >= $line['source']) &&
+                                ($destination <= $line['source'] + $line['range'] - 1)) {
+                                $dest = $line['destination'] + abs($line['source'] - $destination);
+                            }
+                        }
+                        $destination = $dest;
+//
+                    }
+                    if ($minimum === false) {
+                        $minimum = $destination;
+                    }
+                    if ($destination < $minimum) {
+                        $minimum = $destination;
                     }
                 }
-                $destination[$seed] = $dest;
-//                var_dump($key, $seed, $dest);
             }
         }
-        var_dump($destination);
-        var_dump(min($destination));
+        var_dump($minimum);
     }
 }
