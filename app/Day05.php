f489b830df0bb9683174e5260e13215a3b84ccec
MikusR <mikus.rakis@gmail.com>
2023-12-05 09:57:07 +0200

    Day 5 part 1

--- app/Day05.php
+++ app/Day05.php
@@ -19,7 +19,85 @@ class Day05
         $response = $client->request('GET', 'https://adventofcode.com/2023/day/5/input', ['cookies' => $jar]);
         echo '<pre>';
         $data = (string)$response->getBody();
-        $data = preg_split('/\r\n|\r|\n/', $data);
-        var_dump($data);
+//        $data = explode('\n\n', $data);
+//        $data = preg_split('/\r\n|\r|\n/', $data);
+//        $data = 'seeds: 79 14 55 13
+//
+//seed-to-soil map:
+//50 98 2
+//52 50 48
+//
+//soil-to-fertilizer map:
+//0 15 37
+//37 52 2
+//39 0 15
+//
+//fertilizer-to-water map:
+//49 53 8
+//0 11 42
+//42 0 7
+//57 7 4
+//
+//water-to-light map:
+//88 18 7
+//18 25 70
+//
+//light-to-temperature map:
+//45 77 23
+//81 45 19
+//68 64 13
+//
+//temperature-to-humidity map:
+//0 69 1
+//1 0 69
+//
+//humidity-to-location map:
+//60 56 37
+//56 93 4';
+
+        $data = preg_split('/\n\n/', $data);
+        $almanac = [];
+//        preg_match_all('(\d+)', $test[$card]['winners'], $matches);
+        foreach ($data as $key => $d) {
+            if ($key === 0) {
+                $temp = explode('seeds:', $d);
+                preg_match_all('(\d+)', $temp[1], $matches);
+                $seeds = array_map('intval', $matches[0]);
+            } else {
+                $temp = preg_split('/:\n/', $d);
+                $temp[1] = preg_split('/\n/', $temp[1]);
+                foreach ($temp[1] as $t) {
+                    if (strlen($t) > 0) {
+                        preg_match_all('(\d+)', $t, $matches);
+                        if (count($matches) > 0) {
+                            $almanac[$temp[0]][] =
+                                [
+                                    'destination' => (int)$matches[0][0],
+                                    'source' => (int)$matches[0][1],
+                                    'range' => (int)$matches[0][2]
+                                ];
+                        }
+                    }
+                }
+            }
+        }
+        foreach ($seeds as $seed) {
+            $destination[$seed] = $seed;
+            $dest = $seed;
+            foreach ($almanac as $key => $item) {
+                $found = false;
+                foreach ($item as $line) {
+                    if (($destination[$seed] >= $line['source']) &&
+                        ($destination[$seed] <= $line['source'] + $line['range'] - 1)) {
+                        $found = true;
+                        $dest = $line['destination'] + abs($line['source'] - $destination[$seed]);
+                    }
+                }
+                $destination[$seed] = $dest;
+//                var_dump($key, $seed, $dest);
+            }
+        }
+        var_dump($destination);
+        var_dump(min($destination));
     }
 }
