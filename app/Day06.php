b4620fd3ea1ce488527da130e19ea348d54103c7
MikusR <mikus.rakis@gmail.com>
2023-12-06 07:03:30 +0200

    Day 6 init

---
+++ app/Day06.php
@@ -0,0 +1,24 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App;
+
+use GuzzleHttp\Client;
+use GuzzleHttp\Cookie\CookieJar;
+
+class Day06
+{
+    public function run(): void
+    {
+        $client = new Client();
+
+        $jar = CookieJar::fromArray([
+            'session' => $_ENV['AOC_COOKIE']
+        ], 'adventofcode.com');
+        $response = $client->request('GET', 'https://adventofcode.com/2023/day/5/input', ['cookies' => $jar]);
+        echo '<pre>';
+        $data = (string)$response->getBody();
+        var_dump($data);
+    }
+}
--- public/index.php
+++ public/index.php
@@ -2,12 +2,12 @@
 
 declare(strict_types=1);
 
-use App\Day05;
+use App\Day06;
 
 require_once __DIR__ . '/../vendor/autoload.php';
 
 $dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/../');
 $dotenv->safeLoad();
 
-$day = new Day05();
+$day = new Day06();
 $day->run();
