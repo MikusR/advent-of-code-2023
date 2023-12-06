340692d6f423108ca02468abf0d4c89c8eeaec62
MikusR <mikus.rakis@gmail.com>
2023-12-06 07:30:45 +0200

    Day 6 part 1

--- app/Day06.php
+++ app/Day06.php
@@ -19,6 +19,38 @@ class Day06
         $response = $client->request('GET', 'https://adventofcode.com/2023/day/6/input', ['cookies' => $jar]);
         echo '<pre>';
         $data = (string)$response->getBody();
-        var_dump($data);
+        $data2 = 'Time:      7  15   30
+Distance:  9  40  200
+';
+        $raceData = [];
+        $data = preg_split('/\r\n|\r|\n/', $data);
+//        var_dump($data);
+        $temp[0] = explode('Time:', $data[0]);
+        $temp[1] = explode('Distance:', $data[1]);
+
+        preg_match_all('(\d+)', $temp[0][1], $matches[0]);
+        preg_match_all('(\d+)', $temp[1][1], $matches[1]);
+        foreach ($matches[0][0] as $key => $match) {
+            $raceData[$key]['Time'] = $match;
+            $raceData[$key]['Distance'] = $matches[1][0][$key];
+        }
+
+        var_dump($raceData);
+
+        $multiple = 1;
+        foreach ($raceData as $race) {
+            $count = 0;
+            for ($i = 0; $i < $race['Time']; $i++) {
+                $speed = $i;
+                $distance = $i * ($race['Time'] - $i);
+                if ($distance > $race['Distance']) {
+                    $count++;
+//                    echo $race['Distance'] . "|" . $distance . "\n";
+                }
+            }
+            $multiple *= $count;
+            var_dump($count);
+        }
+        var_dump($multiple);
     }
 }
