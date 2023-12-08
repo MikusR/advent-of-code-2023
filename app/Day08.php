73135e7c3443e11cab6149750548c487fe9a5857
MikusR <mikus.rakis@gmail.com>
2023-12-08 07:44:09 +0200

    Day 8 part 1

--- app/Day08.php
+++ app/Day08.php
@@ -17,8 +17,46 @@ class Day08
             'session' => $_ENV['AOC_COOKIE']
         ], 'adventofcode.com');
         $response = $client->request('GET', 'https://adventofcode.com/2023/day/8/input', ['cookies' => $jar]);
-        echo '<pre>';
         $data = (string)$response->getBody();
-        $data2 = '';
+        echo '<pre>';
+        $data2 = 'LR
+
+11A = (11B, XXX)
+11B = (XXX, 11Z)
+11Z = (11B, XXX)
+22A = (22B, XXX)
+22B = (22C, 22C)
+22C = (22Z, 22Z)
+22Z = (22B, 22B)
+XXX = (XXX, XXX)
+';
+        $data = preg_split('/\r\n|\r|\n/', $data);
+        $path = str_split($data[0]);
+        $map = [];
+        unset($data[0]);
+        foreach ($data as $line) {
+            if (strlen($line) > 0) {
+                preg_match('/(\w+) = \((\w+), (\w+)\)/', $line, $matches);
+                $map[$matches[1]] = ['L' => $matches[2], 'R' => $matches[3]];
+            }
+        }
+        $location = 'AAA';
+        $numberOfSteps = 0;
+        $step = 0;
+        $length = count($path);
+        while ($location != 'ZZZ') {
+            $numberOfSteps++;
+            $nextStep = $path[$step];
+            $nextLocation = $map[$location][$nextStep];
+//            var_dump($nextLocation, $location, $path[$step]);
+            $location = $nextLocation;
+
+            if ($length - 1 <= $step) {
+                $step = 0;
+            } else {
+                $step++;
+            }
+        }
+        var_dump($numberOfSteps);
     }
 }
